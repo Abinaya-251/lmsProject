145561a7dac33bc9674f06bd672f59c1f99464a1
shilpa053020 <shilpa053020@gmail.com>
2024-08-12 11:21:08 +0530

    ID: 103 I have integrated email functionality to automatically send confirmation emails when a user registers or purchases a course.

--- server/index.js
+++ server/index.js
@@ -1,29 +1,28 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
-import cors from "cors"
-import adminRouter from "./Routes/adminRoute.js"
-import userRouter from "./Routes/userRoute.js"
+import cors from "cors";
+import adminRouter from "./Routes/adminRoute.js";
+import userRouter from "./Routes/userRoute.js";
 
-const app = express(); 
-dotenv.config()
-app.use(express.json())
-app.use(cors())
-app.use("/admin",adminRouter)
-app.use("/student",userRouter)
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+app.use("/admin", adminRouter);
+app.use("/student", userRouter);
 
-//CONNECT TO DB 
-const connectDb = async()=>{
-    try{
-      await mongoose.connect(process.env.DB)
-      console.log("conntected to DB")
-    }catch(err){
-        console.log("Error while connecting to db ", err)
-    }
-    
-}
+//CONNECT TO DB
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.DB);
+    console.log("conntected to DB");
+  } catch (err) {
+    console.log("Error while connecting to db ", err);
+  }
+};
 //LISTENING TO PORT
-app.listen(process.env.PORT,()=>{
-    console.log(`server is listening on port ${process.env.PORT}`);
-    connectDb()
-})
+app.listen(process.env.PORT, () => {
+  console.log(`server is listening on port ${process.env.PORT}`);
+  connectDb();
+});
--- server/middleware/Auth.js
+++ server/middleware/Auth.js
@@ -6,28 +6,26 @@ dotenv.config();
 const SECRET_KEY = process.env.SECRET_KEY;
 
 export const verificationToken = async (req, res, next) => {
+  // Retrieve the Authorization header from the request
   const authHeader = req.headers.authorization;
-
+  // Check if the Authorization header is present and properly formatted (starts with "Bearer ")
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res
       .status(401)
       .json({ message: "Authorization header missing or malformed" });
   }
-
+  // Extract the token from the Authorization header (the part after "Bearer ")
   const token = authHeader.split(" ")[1];
-
+  //Verify the token using the SECRET_KEY
   jwt.verify(token, SECRET_KEY, (err, value) => {
     if (err) {
       return res.status(401).json({ message: "Invalid token" });
     }
-
-    console.log("Valid User:", value);
-
+    // If the role is 'admin', log it and attach the user's email to req.admin
     if (value.role === "admin") {
-      console.log("Valid Admin:", value);
       req.admin = value.email;
+      // If the role is 'student', log it and attach the user's email to req.student
     } else if (value.role === "student") {
-      console.log("Valid Student:", value);
       req.student = value.email;
     } else {
       return res.status(401).json({ message: "Invalid user role" });
